14408289872661abed1c726c5b7e7ef37351627f
wistein <stein.wm@web.de>
2025-05-19 00:41:16 +0200

    3.6.5 Wakelock option and fixes

---
+++ docs/SQL tourcount sort species list by code.sql
@@ -0,0 +1,26 @@
+/* SQL-script to sort the TourCount species list by code */
+
+ALTER TABLE counts RENAME TO counts_old;
+
+CREATE TABLE counts
+( _id INTEGER,
+  count_f1i int DEFAULT 0,
+  count_f2i int DEFAULT 0,
+  count_f3i int DEFAULT 0,
+  count_pi int DEFAULT 0,
+  count_li int DEFAULT 0,
+  count_ei int DEFAULT 0,
+  name text,
+  code text,
+  notes text DEFAULT "",
+  name_g text,
+  PRIMARY KEY(_id)
+);
+
+INSERT INTO counts (count_f1i, count_f2i, count_f3i, count_pi, count_li, count_ei, name, code, notes, name_g)
+  SELECT count_f1i, count_f2i, count_f3i, count_pi, count_li, count_ei, name, code, notes, name_g
+  FROM counts_old
+  order by code;
+
+DROP TABLE counts_old;
+
